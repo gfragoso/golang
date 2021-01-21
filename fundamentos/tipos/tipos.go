e1c1d12f003c0c38dc8e02df4179d3274a7d5ef6
Gustavo Fragoso <gustavo.fragoso@ifood.com.br>
2021-01-21 12:59:32 -0300

    first commit

---
+++ fundamentos/tipos/tipos.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"reflect"
+)
+
+func main() {
+	// numeros inteiros
+	fmt.Println(1, 2, 1000)
+	fmt.Println("Literal inteiro é", reflect.TypeOf(32000))
+
+	// sem sinal (só positivos)... uint8 uint16 uint32 uint64
+	var b byte = 255
+	fmt.Println("O byte é", reflect.TypeOf(b))
+
+	i1 := math.MaxInt64
+	fmt.Println("O valor maximo mdo int é", i1)
+	fmt.Println("O tipo de i1 é", reflect.TypeOf(i1))
+
+	var i2 rune = 'a' // representa um mapeamento da tabela Unicode (int32)
+	fmt.Println("O rune é", reflect.TypeOf(i2))
+	fmt.Println("O valor da variavel i2", i2)
+
+	var x float32 = 49.99
+	fmt.Println("O tipo de x é", reflect.TypeOf(x))
+	fmt.Println("O tipo literal 49.99 é", reflect.TypeOf(49.99))
+
+	bo := false
+	fmt.Println("O tipo de bo é", reflect.TypeOf(bo))
+	fmt.Println(!bo)
+
+	s1 := "Olá meu nome é Gustavo"
+	fmt.Println(s1 + "!")
+	fmt.Println("O tamanho da string é", len(s1))
+
+	s2 := `Olá
+	meu 
+	nome 
+	é
+	Gustavo`
+
+	fmt.Println(s2 + "!")
+	fmt.Println("O tamanho da string é", len(s2))
+
+	char := 'a'
+	fmt.Println("O tipo de char é", reflect.TypeOf(char))
+	fmt.Println(char)
+}
