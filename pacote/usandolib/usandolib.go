102016c8143747a4e98e1ad184209707e38e7f17
Gustavo Fragoso <gustavo.fragoso@ifood.com.br>
2021-03-29 18:25:23 -0300

    concorrencia

---
+++ pacote/usandolib/usandolib.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gfragoso/goarea"
+	"github.com/gfragoso/goarea/abc"
+)
+
+func main() {
+	fmt.Println(goarea.Circ(4.0))
+	abc.Epa()
+}
---
+++ testes/matematica/matematica_test.go
@@ -0,0 +1,9 @@
+package matematica
+
+import "testing"
+
+const erroPadrao = "Valor esperado %v, mas o resultado encontrado foi %v"
+
+func TestMedia(t *testing.T) {
+
+}
